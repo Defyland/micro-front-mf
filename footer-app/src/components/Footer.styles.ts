3c96fc331f2da1938e6e4862331d691a8638d76b
Defyland <allanflaviosilva@gmail.com>
2024-09-26 11:32:11 -0300

    feat: create a layout and adjust footer component

---
+++ footer-app/src/components/Footer.styles.ts
@@ -0,0 +1,6 @@
+export const Style = {
+  Footer:
+    "h-[7.25rem] border-b border-2 border-grayBorder flex items-center justify-center",
+  Container: "max-w-[1200px] w-[1200px]",
+  GreenLogo: "w-[3.125rem] h-[3.125rem]",
+};
--- footer-app/src/components/Footer.tsx
+++ footer-app/src/components/Footer.tsx
@@ -1,12 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
-
-import React from "react";
+import "tailwindcss/tailwind.css";
+import { Style } from "./Footer.styles";
 
 const Footer = (props) => {
   return (
-    <div>
-      <span>Footer</span>
-    </div>
+    <footer className={Style.Footer}>
+      <div className={Style.Container}>
+        <img
+          src={
+            "https://github.com/user-attachments/assets/0fc46c96-ec07-414d-9a3c-ef1e595720f4"
+          }
+          className={Style.GreenLogo}
+          alt="green logo"
+        />
+      </div>
+    </footer>
   );
 };
 export default Footer;
